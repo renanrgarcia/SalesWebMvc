2cad9058f3532ff8a61aad54f163feaf1d66c6e4
LuisEEduardo <luiseduardolemos282@gmail.com>
2022-01-16 22:35:01 -0300

    Implementing simple search

--- SalesWebMvc/Data/SeedingService.cs
+++ SalesWebMvc/Data/SeedingService.cs
@@ -18,7 +18,7 @@ namespace SalesWebMvc.Data
         {
             if (_context.Department.Any() ||
                 _context.Seller.Any() ||
-                _context.salesRecord.Any())
+                _context.SalesRecord.Any())
             {
                 return; // BD has been seeded 
             }
@@ -70,7 +70,7 @@ namespace SalesWebMvc.Data
             _context.Department.AddRange(d1, d2, d3, d4);
             _context.Seller.AddRange(s1, s2, s3, s4, s5, s6);
 
-            _context.salesRecord.AddRange(
+            _context.SalesRecord.AddRange(
                 r1, r2, r3, r4, r5, r6, r7, r8, r9, r10,
                 r11, r12, r13, r14, r15, r16, r17, r18, r19, r20,
                 r21, r22, r23, r24, r25, r26, r27, r28, r29, r30
